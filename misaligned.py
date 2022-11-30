b2a6a0ceca68528e9dd4f3adcc0d7bf76d2eceeb
Singh Om Prakash (MS/ESX6-XC) <iom2kor@bosch.com>
2022-11-30 19:01:11 +0530

    returning index value to see error in indexing

--- misaligned.py
+++ misaligned.py
@@ -5,9 +5,10 @@ def print_color_map():
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+    last_major_color_index, last_minor_color_index = i, j
+    return len(major_colors) * len(minor_colors), last_major_color_index*last_minor_color_index
 
 
-result = print_color_map()
-assert(result == 25)
+result, last_index_mul = print_color_map()
+assert(result == last_index_mul)
 print("All is well (maybe!)\n")
