0edad618585d2337adac67abffa3411f0b12b96e
Singh Om Prakash (MS/ESX6-XC) <iom2kor@bosch.com>
2022-12-06 01:23:50 +0530

    modifying misaligned.py fixing the allignment issue

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# Keeping all constant
+
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
+major_index_counter, minor_index_counter = 1, 1
+alligment_list = []
--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,26 @@
+from __init__ import *
+
+def catch_last_index():
+    global alligment_list
+    last_index = alligment_list[-1]
+    return last_index
+
+def calculate_allignment_value():
+    global major_index_counter, minor_index_counter
+    return major_index_counter * 5 + minor_index_counter + 1
+
+def print_allignment(major, minor):
+    global major_index_counter, minor_index_counter
+    print(f'{calculate_allignment_value()} | {major} | {minor}')
+    return calculate_allignment_value()
 
 def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-    for i, major in enumerate(major_colors):
-        for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-    last_major_color_index, last_minor_color_index = i, j
-    return len(major_colors) * len(minor_colors), last_major_color_index*last_minor_color_index
-
+    global major_index_counter, minor_index_counter, alligment_list
+    for major in major_colors:
+        for minor in minor_colors:
+            major_index_counter, minor_index_counter = major_colors.index(major), minor_colors.index(minor)
+            alligment_list.append(print_allignment(major, minor))
+    return len(major_colors) * len(minor_colors), catch_last_index()
 
 result, last_index_mul = print_color_map()
 assert(result == last_index_mul)
